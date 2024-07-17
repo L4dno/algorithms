4dd764123e9aaf8cf93e4ed4456bcb23e6382378
Ladno <xmeloff12326@gmail.com>
2024-07-17 17:48:01 +0300

    finished repo setup

--- leetcode_solver/Program.cs
+++ leetcode_solver/Program.cs
@@ -1,11 +1,24 @@
-﻿using lc_49;
+﻿
 namespace leetcode_solver
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Solution.GroupAnagrams(strs);
+            for (int i = 0; i < _0049GroupAnagrams.Tcnt; ++i)
+            {
+                Solution sol = new Solution();
+                var res = sol.GroupAnagrams(_0049GroupAnagrams.Inputs[i].ToArray());
+                if (res == _0049GroupAnagrams.ExpectedOutputs[i])
+                {
+                    Console.WriteLine("test {0} chech!\n", i);
+                }
+                else
+                {
+                    Console.WriteLine("test {0} failed\n", i);
+                    break;
+                }
+            }
         }
     }
 }
--- leetcode_solver/lc_49.cs
+++ leetcode_solver/lc_49.cs
@@ -5,54 +5,65 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Solution
-{
-    const int AlphSize = 26;
-    public int[] CntLetters(string str)
+    public class Solution
     {
-        int[] res = new int[AlphSize];
-        foreach (char ch in str)
+        const int AlphSize = 26;
+        public int[] CntLetters(string str)
         {
-            res[(int)ch - 97] += 1;
+            int[] res = new int[AlphSize];
+            foreach (char ch in str)
+            {
+                res[(int)ch - 97] += 1;
+            }
+            return res;
         }
-        return res;
+
+        public List<List<string>> GroupAnagrams(string[] strs)
+        {
+            var res = new Dictionary<int[], List<string>>();
+
+            foreach (var str in strs)
+            {
+                var tmp = CntLetters(str);
+                if (res.ContainsKey(tmp))
+                {
+                    res[tmp].Add(str);
+                }
+                else
+                {
+                    res[tmp] = new List<string> { str };
+                }
+            }
+            return res.Values.ToList();
+        }
     }
 
-    public List<List<string>> GroupAnagrams(string[] strs)
-    {
-        var res = new Dictionary<int[], List<string>>();
 
-        foreach (var str in strs)
-        {
-            var tmp = CntLetters(str);
-            if (res.ContainsKey(tmp))
-            {
-                res[tmp].Add(str);
-            }
-            else
-            {
-                res[tmp] = new List<string> { str };
-            }
-        }
-        return res.Values.ToList();
-    }
-}
-
-public static class _0049GroupAnagrams
+    public static class _0049GroupAnagrams
 {
-    private static List<string[]> Inputs = new List<string[]>()
+        static public int Tcnt = 3;
+        public static List<string[]> Inputs = new List<string[]>()
     {
         new string[] {"act","pots","tops","cat","stop","hat"},
         new string[] {"x"},
         new string[] {""}
     };
-    private static List<List<List<string>>> ExpectedOutputs = new List<List<List<string>>>()
+        public static List<List<List<string>>> ExpectedOutputs = new List<List<List<string>>>()
     {
         new List<List<string>>
         {
             new List<string> {"hat"},
             new List<string> {"act", "cat"},
             new List<string> {"stop", "post", "tops"}
+        },
+        new List<List<string>>
+        {
+            new List<string> {"x"}
+        },
+        new List<List<string>>
+        {
+            new List<string>{""}
         }
     };
-}
+    }
+

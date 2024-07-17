b7aa5bf7a706bec1e1473a612d4efb90f543b816
Ladno <xmeloff12326@gmail.com>
2024-07-17 16:46:33 +0300

    started repo with git within VS

---
+++ leetcode_solver/Program.cs
@@ -0,0 +1,11 @@
+﻿using lc_49;
+namespace leetcode_solver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.GroupAnagrams(strs);
+        }
+    }
+}
---
+++ leetcode_solver/lc_49.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    const int AlphSize = 26;
+    public int[] CntLetters(string str)
+    {
+        int[] res = new int[AlphSize];
+        foreach (char ch in str)
+        {
+            res[(int)ch - 97] += 1;
+        }
+        return res;
+    }
+
+    public List<List<string>> GroupAnagrams(string[] strs)
+    {
+        var res = new Dictionary<int[], List<string>>();
+
+        foreach (var str in strs)
+        {
+            var tmp = CntLetters(str);
+            if (res.ContainsKey(tmp))
+            {
+                res[tmp].Add(str);
+            }
+            else
+            {
+                res[tmp] = new List<string> { str };
+            }
+        }
+        return res.Values.ToList();
+    }
+}
+
+public static class _0049GroupAnagrams
+{
+    private static List<string[]> Inputs = new List<string[]>()
+    {
+        new string[] {"act","pots","tops","cat","stop","hat"},
+        new string[] {"x"},
+        new string[] {""}
+    };
+    private static List<List<List<string>>> ExpectedOutputs = new List<List<List<string>>>()
+    {
+        new List<List<string>>
+        {
+            new List<string> {"hat"},
+            new List<string> {"act", "cat"},
+            new List<string> {"stop", "post", "tops"}
+        }
+    };
+}

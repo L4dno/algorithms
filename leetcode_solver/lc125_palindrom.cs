c941fb800c76b7dcb5423676af90df4ae1c5ead1
Ladno <xmeloff12326@gmail.com>
2024-08-06 15:21:14 +0300

    added lc 15 36 125

---
+++ leetcode_solver/lc125_palindrom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lc125
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            int n = s.Length;
+
+            for (int l = 0, r = n - 1; l < r; l++, r--)
+            {
+                while (l < r && !char.IsLetterOrDigit(s[l]))
+                    l += 1;
+                while (l < r && !char.IsLetterOrDigit(s[r]))
+                    r -= 1;
+                if (char.ToLower(s[l]) != char.ToLower(s[r]))
+                    return false;
+            }
+            return true;
+        }
+    }
+
+
+}
+
---
+++ leetcode_solver/lc15_threesum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lc15
+{
+    public class Solution
+    {
+        public List<List<int>> ThreeSum(int[] nums)
+        {
+            // m всегда от l??
+            int n = nums.Length;
+            Array.Sort(nums);
+
+            List<List<int>> res = new List<List<int>>();
+
+            for (int l = 0, r = n - 1; l < r;l++)
+            {
+                int m = l + 1;
+                // while r is too large
+                while (m < r && nums[l] + nums[m] + nums[r] > 0) r -= 1;
+                // while l+m are too small
+                while (m < r && nums[l] + nums[m] + nums[r] < 0)
+                {
+                    m += 1;
+                }
+                if (nums[l] + nums[m] + nums[r] == 0)
+                {
+                    res.Add(new List<int> { nums[l], nums[r], nums[m] });
+                    ++l;
+                    r--;
+                }
+            }
+            return res;
+        }
+    }
+
+}
